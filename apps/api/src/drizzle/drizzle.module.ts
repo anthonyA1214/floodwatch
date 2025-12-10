364455128e441d209f524d7a1ef2166a749204d7
Anthony Amiluddin <anthonyamiluddin1@gmail.com>
2025-12-10 14:07:42 +0800

    drizzle setup (#4)

---
+++ apps/api/src/drizzle/drizzle.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { DRIZZLE } from './drizzle-connection';
+import { ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
+import * as schema from './schemas/schema';
+
+@Module({
+  providers: [
+    {
+      provide: DRIZZLE,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const pool = new Pool({
+          connectionString: configService.getOrThrow('DATABASE_URL'),
+          ssl: true,
+        });
+        const db: NodePgDatabase<typeof schema> = drizzle(pool, { schema });
+        return db;
+      },
+    },
+  ],
+  exports: [DRIZZLE],
+})
+export class DrizzleModule {}
---
+++ apps/api/src/drizzle/seed/users.seed.ts
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker';
+import * as bcrypt from 'bcrypt';
+import { db } from './utils/db';
+import * as schema from '../schemas/schema';
+
+export async function seedUsers(length: number) {
+  try {
+    console.log('🌱 Seeding users...');
+
+    const hash = await bcrypt.hash('password123', 10);
+
+    const data = Array.from({ length: length }).map(() => ({
+      username: faker.internet.username(),
+      password: hash,
+    }));
+
+    await db.insert(schema.users).values(data);
+
+    console.log('✅ Users seeded');
+  } catch (err) {
+    console.log('❌ Failed to seed users:', err);
+  }
+}
