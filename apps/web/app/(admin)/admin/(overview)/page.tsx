cb1cb2d9f92b01f2fe77d12a2cd5b1176243b652
panckeyk <tinedagum@gmail.com>
2026-01-15 16:24:41 +0800

    Dashboard UI

--- apps/web/app/(admin)/admin/(overview)/page.tsx
+++ apps/web/app/(admin)/admin/(overview)/page.tsx
@@ -1,22 +1,43 @@
 import FloodAlertsCard from "@/components/admin/dashboard-components/floodalerts-card";
+import TotalReportsCard from "@/components/admin/dashboard-components/totalreports-card";
 import ActiveFloodAlertsCard from "@/components/admin/dashboard-components/activefloodalerts-card";
+import SafetyLocationCard from "@/components/admin/dashboard-components/safetylocation-card";
 
 export default function Dashboard() {
   return (
     // dashboard
 
-    <section className="flex-1 space-y-6 rounded-2xl p-4 bg-[#F8F8F8]">
-      <div>
+    <section className="flex-1 bg-white p-8 rounded-2xl">
+      <h1 className="text-4xl font-bold text-black">
+        Hello, John Doe
+      </h1>
+      <p className="mt-2 text-gray-600">
+        Here’s what is happening with Emergency Aid Hub Today
+      </p>
 
-        <div>
-          <h4 className="text-5xl ">Hello, JohnDoe </h4>
-          {/* <p className="mt-1 text-sm text-gray-500">
-            Here's what is happening with Emergency Aid Hub Today
-          </p> */}
-        </div>
+      {/* Cards row */}
+      <div className="mt-8 flex gap-6">
+        <FloodAlertsCard />
+        <TotalReportsCard />
       </div>
 
+      {/* Active Flood Alert */}
+      <div className="mt-4 rounded-xl border border-gray-300 bg-white p-6">
+        <h1 className="mb-4 text-xl">
+          Active Flood Alerts
+        </h1>
 
+        <ActiveFloodAlertsCard />
+      </div>
+
+      {/* Safety Location */}
+      <div className="mt-4 rounded-xl border border-gray-300 bg-white p-6">
+        <h1 className="mb-4 text-xl">
+          Safety Location
+        </h1>
+
+        <SafetyLocationCard />
+      </div>
     </section>
   );
 }
--- apps/web/components/admin/dashboard-components/totalreports-card.tsx
+++ apps/web/components/admin/dashboard-components/totalreports-card.tsx
@@ -1,10 +1,10 @@
-export default function FloodAlertsCard() {
+export default function TotalReportsCard() {
     return (
         <div className="flex-1 rounded-xl border border-gray-300 bg-white p-6">
             <div className="flex items-center gap-4">
-                <div className="flex h-12 w-12 items-center justify-center rounded-full bg-blue-500" />
+                <div className="flex h-12 w-12 items-center justify-center rounded-full bg-yellow-300" />
                 <span className="text-xl font-medium text-gray-800">
-                    Flood Alerts
+                    Total Reports
                 </span>
             </div>
 
