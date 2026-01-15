7d58b4617a108b2c65d436c132e4617d04336b0f
panckeyk <tinedagum@gmail.com>
2026-01-15 14:20:31 +0800

    safety guides section

--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -1,7 +1,13 @@
+import SafetyGuidesSection from "@/components/landing/safetyguides-section";
+import LatestNewsReport from "@/components/landing/latestnewsreport-section";
+
+
 export default function Page() {
   return (
     <div className="flex flex-col max-w-356 mx-auto px-4 py-8 md:py-0 pt-16">
       {/* dito ilalagay ung mga sections */}
+      <SafetyGuidesSection/>
+      <LatestNewsReport/>
     </div>
   );
 }
