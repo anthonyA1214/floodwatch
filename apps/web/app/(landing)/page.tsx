97a927cf997b7ef72bdb43a738dfa0be0ab4a506
panckeyk <tinedagum@gmail.com>
2026-01-15 21:49:00 +0800

    safety guide section fix

--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -1,13 +1,10 @@
 import SafetyGuidesSection from "@/components/landing/safetyguides-section";
-import LatestNewsReport from "@/components/landing/latestnewsreport-section";
-
 
 export default function Page() {
   return (
     <div className="flex flex-col max-w-356 mx-auto px-4 py-8 md:py-0 pt-16">
       {/* dito ilalagay ung mga sections */}
       <SafetyGuidesSection/>
-      <LatestNewsReport/>
     </div>
   );
 }
