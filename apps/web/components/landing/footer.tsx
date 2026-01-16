ca23db5333c5863cc35532c7ad640e8f341a9e22
anthonyA1214 <anthonyamiluddin1@gmail.com>
2026-01-16 21:20:07 +0800

    improvements

--- apps/web/components/landing/footer.tsx
+++ apps/web/components/landing/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-[#2F327D] mt-auto">
       <div className="max-w-356 mx-auto px-4 py-16">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 sm:gap-8 md:gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-8">
           {/* Logo + Description */}
           <div className="flex gap-5 items-start">
             <Image
@@ -16,51 +16,55 @@ export default function Footer() {
             />
             <div className="space-y-3">
               <h3 className="text-2xl text-white font-semibold">Flood Watch</h3>
-              <p className="text-white/80 text-sm leading-relaxed">
+              <p className="text-white/80 text-sm">
                 Partner with Us. Help FloodWatch protect communities everywhere.
               </p>
             </div>
           </div>
 
-          {/* Contacts */}
-          <div className="flex flex-col text-white gap-4">
-            <h2 className="text-xl font-semibold">Contacts</h2>
-            <span className="text-white/80 text-sm">0969-512-6532</span>
-            <span className="text-white/80 text-sm">floodwatch@gmail.com</span>
-            <span className="text-white/80 text-sm leading-relaxed">
-              University of Caloocan City, Congressional Road
-            </span>
-          </div>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8 md:gap-12 col-span-3">
+            {/* Contacts */}
+            <div className="flex flex-col text-white gap-4">
+              <h2 className="text-xl font-semibold">Contacts</h2>
+              <span className="text-white/80 text-sm">0969-512-6532</span>
+              <span className="text-white/80 text-sm">
+                floodwatch@gmail.com
+              </span>
+              <span className="text-white/80 text-sm leading-relaxed">
+                University of Caloocan City, Congressional Road
+              </span>
+            </div>
 
-          {/* Quick Links */}
-          <div className="flex flex-col text-white gap-4">
-            <h2 className="text-xl font-semibold">Quick Links</h2>
-            <span className="text-white/80 text-sm hover:text-white cursor-pointer">
-              About
-            </span>
-            <span className="text-white/80 text-sm hover:text-white cursor-pointer">
-              Partners
-            </span>
-            <span className="text-white/80 text-sm hover:text-white cursor-pointer">
-              Contact
-            </span>
-          </div>
+            {/* Quick Links */}
+            <div className="flex flex-col text-white gap-4">
+              <h2 className="text-xl font-semibold">Quick Links</h2>
+              <span className="text-white/80 text-sm hover:text-white cursor-pointer">
+                About
+              </span>
+              <span className="text-white/80 text-sm hover:text-white cursor-pointer">
+                Partners
+              </span>
+              <span className="text-white/80 text-sm hover:text-white cursor-pointer">
+                Contact
+              </span>
+            </div>
 
-          {/* Social Media */}
-          <div className="flex flex-col text-white gap-4">
-            <h2 className="text-xl font-semibold">Social Media</h2>
-            <span className="text-white/80 text-sm hover:text-white cursor-pointer">
-              Facebook
-            </span>
-            <span className="text-white/80 text-sm hover:text-white cursor-pointer">
-              Twitter
-            </span>
-            <span className="text-white/80 text-sm hover:text-white cursor-pointer">
-              Instagram
-            </span>
-            <span className="text-white/80 text-sm hover:text-white cursor-pointer">
-              Indeed
-            </span>
+            {/* Social Media */}
+            <div className="flex flex-col text-white gap-4">
+              <h2 className="text-xl font-semibold">Social Media</h2>
+              <span className="text-white/80 text-sm hover:text-white cursor-pointer">
+                Facebook
+              </span>
+              <span className="text-white/80 text-sm hover:text-white cursor-pointer">
+                Twitter
+              </span>
+              <span className="text-white/80 text-sm hover:text-white cursor-pointer">
+                Instagram
+              </span>
+              <span className="text-white/80 text-sm hover:text-white cursor-pointer">
+                Indeed
+              </span>
+            </div>
           </div>
         </div>
 
@@ -72,8 +76,7 @@ export default function Footer() {
         {/* Bottom */}
         <div className="m-auto">
           <h2 className="text-white">
-            {' '}
-            © 2026 Company Name. All rights reserve{' '}
+            © 2026 AidLink Tech. All rights reserved.
           </h2>
         </div>
       </div>
--- apps/web/components/landing/latest-news-report-section.tsx
+++ apps/web/components/landing/latest-news-report-section.tsx
@@ -2,8 +2,8 @@ import LatestNewsCard from './latest-news-card';
 
 export default function LatestNewsReport() {
   return (
-    <section className="mt-16 font-['Roboto']">
-      <div className="max-w-6xl mx-auto bg-white rounded-2xl px-8 py-12 h-[500px]">
+    <section className="pb-20">
+      <div className="max-w-356 mx-auto bg-white rounded-2xl px-8 py-12 h-[500px]">
         {/* Title */}
         <h2 className="text-3xl font-bold text-center">
           <span className="text-[#2E2F35]">Latest </span>
