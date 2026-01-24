423d0eac6dde9eac067afa463d9d4cb95b36d2fc
Anthony Amiluddin <anthonyamiluddin1@gmail.com>
2026-01-24 09:41:40 +0800

    Refactor/fe landing (#12)
    * refactor landing page
    
    * fixes

--- apps/web/app/config/fonts.ts
+++ apps/web/app/config/fonts.ts
@@ -1,7 +1,12 @@
-import { Inter } from 'next/font/google';
+import { Inter, Poppins } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin'],
-  weight: ['400', '700'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   variable: '--font-inter',
+});
+export const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-poppins',
 });
---
+++ apps/web/components/landing/sections/latest-news-report-section.tsx
@@ -0,0 +1,32 @@
+import LatestNewsCard from '../latest-news-card';
+export default function LatestNewsReport() {
+  return (
+    <section id="latest-news">
+      <div className="flex flex-col gap-6 md:gap-10 py-20 max-w-7xl mx-auto px-4">
+        <div className="space-y-2">
+          {/* Title */}
+          <h2 className="font-poppins text-3xl sm:text-4xl md:text-5xl font-semibold text-center">
+            Latest <span className="text-[#2F327D]">News Report</span>
+          </h2>
+
+          {/* Subtitle */}
+          <p className="text-lg sm:text-xl text-gray-600 text-center">
+            Be updated on latest news for the whole week
+          </p>
+        </div>
+
+        {/* News Cards */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8">
+          <div>
+            <LatestNewsCard variant="featured" src="/images/chloe.jfif" />
+          </div>
+          <div className="flex flex-col gap-6">
+            <LatestNewsCard src="/images/luna.jfif" />
+            <LatestNewsCard src="/images/tobi.jfif" />
+            <LatestNewsCard src="/images/kazy.jfif" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
