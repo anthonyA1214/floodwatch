a498ea0c6fbffaaaa1f94b652559c59f6b1d0ee5
anthonyA1214 <anthonyamiluddin1@gmail.com>
2026-02-02 14:23:13 +0800

    fixes

--- apps/web/app/(map)/map/page.tsx
+++ apps/web/app/(map)/map/page.tsx
@@ -1,10 +1,17 @@
-import Searchbar from '@/components/map/searchbar';
-import MapLegend from '@/components/map/maplegend';
-export default function InteractiveMap() {
+'use client';
+
+import SearchBar from '@/components/map/search-bar';
+import MapLegend from '@/components/map/map-legend';
+import InteractiveMap from '@/components/map/interactive-map';
+import { useState } from 'react';
+export default function InteractiveMapPage() {
+  const [showLegend, setShowLegend] = useState(false);
+
   return (
-    <>
-      <Searchbar />
-      <MapLegend />
-    </>
+    <div className="relative w-full h-full">
+      <SearchBar toggleLegend={() => setShowLegend(!showLegend)} />
+      <MapLegend show={showLegend} />
+      <InteractiveMap />
+    </div>
   );
 }
