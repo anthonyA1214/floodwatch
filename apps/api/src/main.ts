c9061367f5041c50a8e48edadd872038b1c18b4a
Anthony Amiluddin <anthonyamiluddin1@gmail.com>
2026-02-03 20:17:41 +0800

    Feat/be logger setup (#19)
    * logger
    
    * updated turbo

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,13 +4,16 @@ import * as dotenv from 'dotenv';
 import { ConfigService } from '@nestjs/config';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const port = process.env.PORT ?? 3000;
 
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
   app.use(helmet());
   app.use(cookieParser());
 
