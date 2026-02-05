d43287c23b27de3735e0fee35dac986ac0a22950
anthonyA1214 <anthonyamiluddin1@gmail.com>
2026-02-05 16:21:50 +0800

    fix

--- packages/schemas/src/users.ts
+++ packages/schemas/src/users.ts
@@ -1,5 +1,16 @@
 import { z } from 'zod';
 
+export const usersSchema = z.object({
+  id: z.number(),
+  email: z.email(),
+  role: z.enum(['admin', 'user']),
+  status: z.enum(['active', 'blocked']),
+  name: z.string().min(1),
+  profilePicture: z.string().optional(),
+  homeAddress: z.string().optional(),
+  joinDate: z.string(),
+});
+
 export const getMeSchema = z.object({
   id: z.number(),
   email: z.email(),
@@ -13,4 +24,5 @@ export const getMeSchema = z.object({
   createdAt: z.string(),
 });
 
+export type UsersDto = z.infer<typeof usersSchema>;
 export type GetMeDto = z.infer<typeof getMeSchema>;
