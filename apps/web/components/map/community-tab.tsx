c48c4e5b9c39c04794780008b3b7f6f48b5537ef
anthonyA1214 <anthonyamiluddin1@gmail.com>
2026-03-04 21:49:56 +0800

    fix error

--- apps/web/components/map/community-tab.tsx
+++ apps/web/components/map/community-tab.tsx
@@ -1,25 +1,9 @@
-import PostComposer from '@/components/shared/comment-composer';
-import PostCard from '@/components/shared/comment-card';
+import CommentComposer from '@/components/shared/comment-composer';
 
 export default function CommunityTab() {
   return (
     <div className="flex flex-col gap-6">
-      <PostComposer />
-
-      <PostCard
-        author={{ name: 'Pedro Santos' }}
-        content="Volunteers are needed to help with sandbagging efforts in flood-prone areas. Please contact the local barangay office if you can assist."
-        timestamp="1 day ago"
-        reportCount={2}
-      />
-
-      <PostCard
-        author={{ name: 'Juan Dela Cruz' }}
-        content="Heavy rainfall in Zapote area, its starting to accumulate water. Please be careful if you're heading this way! #Flood"
-        imageUrl="/images/before_flood_image.jpg"
-        timestamp="2 hrs ago"
-        reportCount={3}
-      />
+      <CommentComposer />
     </div>
   );
 }
