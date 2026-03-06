c2530d6711bfc06e8bc0d32d950e9a2a335ed787
anthonyA1214 <anthonyamiluddin1@gmail.com>
2026-03-06 22:12:08 +0800

    final fix

--- apps/web/components/map/community-tab.tsx
+++ apps/web/components/map/community-tab.tsx
@@ -2,8 +2,8 @@ import CommentComposer from '@/components/shared/comment-composer';
 
 export default function CommunityTab() {
   return (
-    <div className="flex flex-col gap-6">
-      <CommentComposer />
+    <div className='flex flex-col gap-6'>
+      <CommentComposer reportId={100000} />
     </div>
   );
 }
